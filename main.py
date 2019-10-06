f25b791c2e23bed1324af9d081ffe75a43e73fc2
Hawkeye <realisvenator@gmail.com>
2019-10-05 19:00:16 -0500

    File stucture
    Just added where the user can find the list of supported commands

--- main.py
+++ main.py
@@ -13,18 +13,26 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 @client.event
 async def on_message(message):
-    if message.content.startswith('$greet'):
-        channel = message.channel
-        await channel.send('Say hello!')
+    # if message.content.startswith('$greet'):
+    #     channel = message.channel
+    #     await channel.send('Say hello!')
 
-        def check(m):
-            return m.content == 'hello' and m.channel == channel
+    #     def check(m):
+    #         return m.content == 'hello' and m.channel == channel
 
-        msg = await client.wait_for('message', check=check)
-        await channel.send('Hello {.author}!'.format(msg))
+    #     msg = await client.wait_for('message', check=check)
+    #     await channel.send('Hello {.author}!'.format(msg))
     if message.content.startswith('$lulw'):
         chanel = message.channel
         await chanel.send('Sick meme bruh')
+
+    if message.content.startswith('`creator'):
+        chanel = message.channel
+        await chanel.send('I am created by Hawkeye. You can find more about me here: https://github.com/H4wki/SyntaxrBot/tree/master')
+
+    if message.content.startswith('`help'):
+        target = message.author
+        await target.send("Hi there, I here you are looking for some instructions on my instructions. You can find a list of commands here: ")
 
 getToken = open("token.token", "r")
 token = getToken.read()
